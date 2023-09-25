d98764c8d23828834c0149b7fd2e8d55118735e0
Diogo Pinto <dpintobarbosa@gmail.com>
2023-09-25 19:18:33 -0300

    resolve requisito 4

--- src/3-out-of-stock-products.js
+++ src/3-out-of-stock-products.js
@@ -3,7 +3,7 @@ const stockProducts = require('./data.json');
 const getOutOfStockProducts = () => {
   let productsOutOfStock = [];
   for (let index = 0; index < stockProducts.length; index += 1) {
-    
+
     if (stockProducts[index].quantityInStock === 0) {
       productsOutOfStock.push(stockProducts[index].productName);
     }
--- src/4-low-stock-products.js
+++ src/4-low-stock-products.js
@@ -1,7 +1,16 @@
 const stockProducts = require('./data.json');
 
+
 const getLowStockProducts = () => {
-  // Desenvolva seu código dentro dessa função...
+  let stockQuantity = [];
+  for (let index = 0; index < stockProducts.length; index += 1) {
+
+    if (stockProducts[index].quantityInStock > 0 && stockProducts[index].quantityInStock <= 10) {
+      stockQuantity.push(`${stockProducts[index].productName}: ${stockProducts[index].quantityInStock} unidades`);
+    }
+
+  }
+  return stockQuantity;
 };
 
 module.exports = { getLowStockProducts };
