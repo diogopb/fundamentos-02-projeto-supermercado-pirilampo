57d185d7cfd0d20cd34572c3e83302268c8b5192
Diogo Pinto <dpintobarbosa@gmail.com>
2023-09-26 09:28:38 -0300

    resolve requisito 8

--- src/8-products-on-sale.js
+++ src/8-products-on-sale.js
@@ -1,7 +1,20 @@
 const stockProducts = require('./data.json');
 
 const getProductsOnSale = () => {
-  // Desenvolva seu código dentro dessa função...
+  let findProducts;
+  let findSale = [];
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].onSale === true) {
+      let priceProducts = stockProducts[index].price;
+      findProducts = {
+        description: stockProducts[index].description,
+        formattedPrice: `R$ ${priceProducts.toFixed(2)}`,
+        onSale: stockProducts[index].onSale,
+      }
+      findSale.push(findProducts);
+    }
+  }
+  return findSale;
 };
 
 module.exports = { getProductsOnSale };
