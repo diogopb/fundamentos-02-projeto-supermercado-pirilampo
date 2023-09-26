d7a8cad103c6abceff1214ae333421dda4988d20
Diogo Pinto <dpintobarbosa@gmail.com>
2023-09-26 10:02:41 -0300

    resolve requisito 9

--- src/9-products-with-allergy-or-intolerance-info.js
+++ src/9-products-with-allergy-or-intolerance-info.js
@@ -1,7 +1,20 @@
 const stockProducts = require('./data.json');
 
 const getProductsWithAllergyOrIntoleranceInfo = () => {
-  // Desenvolva seu código dentro dessa função...
+  let findProducts;
+  let findAlergyproducts = [];
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].allergyOrIntolerance) {
+      let priceProducts = stockProducts[index].price;
+      findProducts = {
+        description: stockProducts[index].description,
+        formattedPrice: `R$ ${priceProducts.toFixed(2)}`,
+        allergyOrIntoleranceMessage: `Pode conter: ${stockProducts[index].allergyOrIntolerance.join(' ')}`,
+      }
+      findAlergyproducts.push(findProducts);
+    }
+  }
+  return findAlergyproducts;
 };
 
 module.exports = { getProductsWithAllergyOrIntoleranceInfo };
