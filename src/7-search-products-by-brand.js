5c79cdcaea55f177422abd0672828ae15da8d6fc
Diogo Pinto <dpintobarbosa@gmail.com>
2023-09-26 09:10:56 -0300

    resolve requisito 7

--- src/7-search-products-by-brand.js
+++ src/7-search-products-by-brand.js
@@ -1,7 +1,20 @@
 const stockProducts = require('./data.json');
 
-const searchProductsByBrand = () => {
-  // Desenvolva seu código dentro dessa função...
+const searchProductsByBrand = (item) => {
+  let findProducts;
+  let findBrands = [];
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].brand === item) {
+      let priceProducts = stockProducts[index].price;
+      findProducts = {
+        description: stockProducts[index].description,
+        formattedPrice: `R$ ${priceProducts.toFixed(2)}`,
+      }
+      findBrands.push(findProducts);
+    }
+  }
+  return findBrands;
 };
+
 
 module.exports = { searchProductsByBrand };
